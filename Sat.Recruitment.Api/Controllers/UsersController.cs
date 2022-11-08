4cc58ec478e021c66f19b9af6f1516138714b4bf
DESKTOP-5UD9IRO\Valeria <valeria.arrieta@cgtscorp.es>
2022-11-08 20:31:52 -0300

    Add logger in UsersController

--- Sat.Recruitment.Api/Controllers/UsersController.cs
+++ Sat.Recruitment.Api/Controllers/UsersController.cs
@@ -2,6 +2,8 @@
 using System.Threading.Tasks;
 using MediatR;
 using Sat.Recruitment.Core.Commands.CreateUser;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 
 namespace Sat.Recruitment.Api.Controllers
 {
@@ -10,15 +12,18 @@ namespace Sat.Recruitment.Api.Controllers
     public class UsersController : ControllerBase
     {
         private readonly IMediator _mediator;
+        private readonly ILogger<UsersController> _logger;
 
-        public UsersController(IMediator mediator)
+        public UsersController(IMediator mediator,ILogger<UsersController> logger)
         {
             _mediator = mediator;
+            _logger = logger;
         }
 
         [HttpPost]
         public async Task<IActionResult> CreateUser([FromBody] CreateUserRequest request)
         {
+            _logger.LogInformation($"CreateUserRequest - {JsonConvert.SerializeObject(request)}");
             var result = await _mediator.Send(new CreateUserCommand { CreateUserRequest = request });
 
             if (result.IsSuccess)
